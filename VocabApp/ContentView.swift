2dec4692895eb141d0fa6ff4f51c469d5a89eac7
typicel <tyler.mccormick@colorado.edu>
2024-05-24 21:33:00 -0600

    Initial Commit

---
+++ VocabApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VocabApp
+//
+//  Created by Tyler McCormick on 5/24/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VocabApp/VocabAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VocabAppApp.swift
+//  VocabApp
+//
+//  Created by Tyler McCormick on 5/24/24.
+//
+
+import SwiftUI
+
+@main
+struct VocabAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
